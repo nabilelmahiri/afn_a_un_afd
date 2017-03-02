bff9b50c7c1aa18f468e066f01325020c8813888
sconabilos <nabil-sco-tae@hotmail.fr>
2017-03-02 03:36:13 +0000

    Initial commit

---
+++ src/afntoafd/Main.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package afntoafd;
+
+import afntoafdd.view.screen;
+
+/**
+ *
+ * @author EL Mahiri Nabil
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new screen().setVisible(true);
+    }
+}
